a0e04e8f68131cea02f2cd2c0673305be195f91a
keith <keith.munoz@pi.edu.co>
2026-05-19 12:58:21 -0500

    creo estructura backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require ('express')
 
 const app = express()
 
+app.get('/', (req,res) => {
+    res.send('hola mi perroooo')
+})
+
 app.listen(4000,()=>{
     console.log ('el servidor esta en el puerto 4000')
 })
