d3e19bc52be967841f0fcb9b1c6baf0a3f89423a
Keith <elkitsuarez@gmail.com>
2026-07-02 16:29:59 -0500

    agregue imagenes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,23 +1,34 @@
 fetch('http://localhost:4000/api/productos')
-.then(datos => datos.json()).then(data=>{
-     console.log(data)
-    let publicacion = document.getElementById('cont')
-    for(i=0;i<=5;i++){
-    
-    publicacion.innerHTML += `<div class="publicacion">
-                <img class="img-pub" src="assets/${ data.productos[i].imagen }" alt="">
-                <h1>${data.productos[i].nombre}</h1>        
+.then(res => res.json())
+.then(data => {
+
+    console.log(data);
+
+    let contenedor = document.getElementById('cont');
+
+    contenedor.innerHTML = ""; // limpia antes
+
+    data.productos.forEach(producto => {
+
+        contenedor.innerHTML += `
+            <div class="publicacion">
+
+                <img class="img-pub" src="assets/${producto.imagen}" alt="">
+
+                <h2>${producto.nombre}</h2>
+
+                <p class="precio">$${producto.precio ?? "0.00"}</p>
+
                 <div class="bottones">
-                    <button>comprar</button>
-                <button id="boton">agregar a carrito</button>
+                    <button>Comprar</button>
+                    <button>Agregar</button>
                 </div>
-             </div>`
-    
-    }
-})
 
+            </div>
+        `;
+    });
 
-
+});
 
 
 
